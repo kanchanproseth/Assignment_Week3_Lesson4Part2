42a7ba9030e92ba9992f7039521aab4d747aa179
kanchanproseth <kchanproseth@gmail.com>
2023-11-05 19:21:49 -0600

    Initial commit

---
+++ app/src/main/java/com/cs473/mda/assignmentweek3lesson4part2/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cs473.mda.assignmentweek3lesson4part2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cs473.mda.assignmentweek3lesson4part2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Assignment Week 3 Lesson 4 Part 2"
+include(":app")
